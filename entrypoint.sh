dd7199e5b7c54300cc02e9011177364c989081aa
Webber <webber@takken.io>
2020-09-27 21:50:52 +0200

    Add intructions for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,3 +13,29 @@ xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
     -quit \
     -returnlicense
 
+# Catch exit code
+UNITY_EXIT_CODE=$?
+
+#
+# Instructions for debugging
+#
+
+if [[ $UNITY_EXIT_CODE -gt 0 ]]; then
+echo ""
+echo "###########################"
+echo "#         Failure         #"
+echo "###########################"
+echo ""
+echo "Please note that the exit code is not very descriptive."
+echo "Most likely it will not help you solve the issue."
+echo ""
+echo "To find the reason for failure: please search for errors in the log above."
+echo ""
+exit $UNITY_EXIT_CODE
+fi;
+
+#
+# Exit with code from the return-license step.
+#
+
+exit $UNITY_EXIT_CODE
